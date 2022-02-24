cc047fe0870c7dc01ce0ee4066257dbce31c068b
Joss Dixon <josscdixon@gmail.com>
2022-02-24 09:34:33 +0000

    fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # The below commented out to prevent landing page authentication
+  # before_action :authenticate_user!
 end
